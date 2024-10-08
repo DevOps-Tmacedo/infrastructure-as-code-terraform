80b41df47662f75ae5a7edda5188ea5af4812126
Thiago Nones <thiagononesmacedo@gmail.com>
2024-10-07 21:46:59 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the project"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Path to the service account JSON file"
+  type        = string
+}
